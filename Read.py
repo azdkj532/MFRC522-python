f0ec1fbc6090a92a7c878d83634c9f4a65a15306
Squirrel <kanade123321@gmail.com>
2016-08-13 02:50:08 +0800

    remove dummy code

--- Read.py
+++ Read.py
@@ -41,7 +41,6 @@ while continue_reading:
     if status == MIFAREReader.MI_OK:
 
         # Print UID
-        print "Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3])
         print("Card UID [%s, %s, %s, %s]" % (str(uid[0]), str(uid[1]), str(uid[2]), str(uid[3])))
     
         # This is the default key for authentication
